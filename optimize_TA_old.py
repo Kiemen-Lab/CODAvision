f8a19178739e19a5885261b325076b1e2f762345
Becerril42 <jgomezb2@jh.edu>
2024-04-04 15:26:36 -0400

    optimize_TA_old finished

--- optimize_TA_old.py
+++ optimize_TA_old.py
@@ -1,11 +1,11 @@
 import os
 import numpy as np
-import matplotlib.pyplot as plt
-from skimage import io, morphology, measure
+from skimage import io, morphology
+from skimage.morphology import remove_small_objects
+import cv2
 
 def optimize_TA_old(pth, imnm): # pth: path to image folder\        #imnm: image name
-    outpth = os.path.join(pth, 'TA/')
-    print('a')
+    outpth = os.path.join(pth.rstrip('\\'), 'TA')
     if not os.path.isdir(outpth):
         os.mkdir(outpth)
     try:
@@ -21,13 +21,13 @@ def optimize_TA_old(pth, imnm): # pth: path to image folder\        #imnm: image
         return im0, TA, outpth
 
     TA = im0[:, :, 1] < 210
-    TA = cv2.morphologyEx(TA.astype(np.uint8), cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
-    TA = cv2.morphologyEx(TA.astype(np.uint8), cv2.MORPH_OPEN, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
-    _, TA = cv2.threshold(TA, 127, 255, cv2.THRESH_BINARY)
-
-    cv2.imwrite(os.path.join(outpth, imnm + '.tif'), TA)
+    kernel_size = 3
+    kernel = morphology.disk(kernel_size)
+    TA = morphology.binary_closing(TA, kernel)
+    min_area = 4
+    TA = remove_small_objects(TA, min_size=min_area)
+    cv2.imwrite(os.path.join(outpth, imnm + '.tif'), TA.astype(np.uint8))
     return im0, TA, outpth
 
-
-imnm = '84 - 2024-02-26 10.33.40'
-optimize_TA_old(r'\\10.99.68.52\Kiemendata\Valentina Matos\LG HG PanIN project\Jaime\Python tests\'', imnm)
+imnm = '2024-02-26 10.36.39'
+pth = r'\\10.99.68.52\Kiemendata\Valentina Matos\LG HG PanIN project\Jaime\Python tests'
