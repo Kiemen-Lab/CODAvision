bb4e92f346df9179cfe54b4d0eb5f3d0169596e9
Valentina <vmatosr1@jh.edu>
2024-04-19 14:13:42 -0400

    Code reads an image and returns the image as a numpy array and a binary copy of the image's green value after it has been thresholded.

---
+++ calculate_tissue_mask.py
@@ -0,0 +1,62 @@
+import os
+import numpy as np
+from skimage import io, morphology
+import cv2
+import scipy
+from skimage.morphology import remove_small_objects
+import pickle
+
+def calculate_tissue_mask(pth, imnm):
+    """
+        Reads an image and returns the image as a numpy array and a binary copy of the image's green value after it has been thresholded.
+
+        Parameters:
+            - pth (str): The path where the images are located.
+            - imnm (str): The name of the image.
+
+        Returns:
+            - im0 (np.ndarray): The image as a numpy array.
+            - outpth (str): The path where TA has been saved.
+           """
+    outpth = os.path.join(pth.rstrip('\\'), 'TA')
+    if not os.path.isdir(outpth):
+        os.mkdir(outpth)
+    try:
+        im0 = io.imread(os.path.join(pth, imnm + '.tif'))
+    except:
+        try:
+            im0 = io.imread(os.path.join(pth, imnm + '.jpg'))
+        except:
+            im0 = io.imread(os.path.join(pth, imnm + '.jp2'))
+    if os.path.isfile(os.path.join(outpth, imnm + '.tif')):
+        TA = cv2.imread(os.path.join(outpth, imnm + '.tif'), cv2.IMREAD_GRAYSCALE)
+        print('Existing TA loaded')
+        return im0, TA, outpth
+
+    print('     Calculating TA image')
+    if os.path.isfile(os.path.join(outpth, 'TA_cutoff.mat')):
+        data = scipy.io.loadmat(os.path.join(outpth, 'TA_cutoff.mat'))
+        #with open(os.path.join(outpth, 'TA_cutoff.pkl'), 'rb') as f: # todo: uncomment these two lines and delete the one avobe when TA_cutoff.plk has been created
+            #data = pickle.load(f)      # todo: check if bugs arise after changing .mat file for .pkl file
+        cts = data['cts']
+        ct=0
+        for i in cts:
+            for j in i:
+                ct += j
+            ct = ct/len(i)
+    else:
+        ct = 210
+
+    TA = im0[:, :, 1] < ct
+    kernel_size = 3
+    kernel = morphology.disk(kernel_size)
+    TA = morphology.binary_closing(TA, kernel)
+    min_area = 10
+    TA = remove_small_objects(TA, min_size=min_area)
+    cv2.imwrite(os.path.join(outpth, imnm + '.tif'), TA.astype(np.uint8))
+    return im0, TA, outpth
+
+# Example
+# imnm = '84 - 2024-02-26 10.33.40'
+# pth = r'\\10.99.68.52\Kiemendata\Valentina Matos\LG HG PanIN project\Jaime\Python tests'
+# calculate_tissue_mask(pth, imnm)
