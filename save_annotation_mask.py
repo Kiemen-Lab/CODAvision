025c29cd1f7042dbb5dd57a30f66a0691cfa6dc8
Becerril42 <jgomezb2@jh.edu>
2024-04-22 16:12:47 -0400

    Save_annotation_mask now reads pkl files

--- save_annotation_mask.py
+++ save_annotation_mask.py
@@ -1,9 +1,15 @@
+
+import format_white
 import os
 from skimage import io
 import numpy as np
 from scipy.ndimage import label, binary_fill_holes
 import scipy
-import format_white
+import calculate_tissue_mask
+from IPython.display import display
+import pandas as pd
+from skimage.morphology import remove_small_objects
+import pickle
 
 def save_annotation_mask(I,outpth,WS,umpix,TA,kpb=0):  #I is the image in matrix form
     if umpix == 100:
@@ -15,18 +21,17 @@ def save_annotation_mask(I,outpth,WS,umpix,TA,kpb=0):  #I is the image in matrix
     print('     2. of 4. Interpolating annotated regions and saving mask image')
     num = len(WS[0])
     try:
-        xyout = scipy.io.loadmat(outpth + 'annotations.mat')['xyout']
+        with open(os.path.join(outpth, 'annotations.pkl'), 'rb') as f:
+            data = pickle.load(f)
+            xyout = data['xyout']
         if xyout.size == 0:
             J = np.zeros(I.size)
         else:
             xyout[:,2:3] = round(xyout[:,2:3]/umpix)
-
             if TA.size > 0:
                 TA = TA > 0
-                TA = label(TA, np.ones((3, 3)))[0] >= 30
-                TA = 255 - TA  # if TA saved as uint8 (current format from python)
-
-
+                TA = remove_small_objects(TA.astype(bool), min_size=30, connectivity=2)
+                TA = np.logical_not(TA)
             else:
                 I = I.astype(float)
                 TA1 = np.std(I[:, :, [0, 1]], 2, ddof=1)
@@ -50,37 +55,31 @@ def save_annotation_mask(I,outpth,WS,umpix,TA,kpb=0):  #I is the image in matrix
                     if pp == 0:
                         continue
                     cc = np.flatnonzero(xyz[:, 1] == pp)
-
                     xyv = np.vstack((xyz[cc, 2:4], xyz[cc[0], 2:4]))
-                    dxyv = np.sqrt(np.sum((np.roll(xyv, -1, axis=0) - xyv[:-1, :]) ** 2, axis=1))
-
-                    xyv = xyv[~np.isclose(dxyv, 0), :]
-                    dxyv = dxyv[~np.isclose(dxyv, 0)]
+                    dxyv = np.sqrt(np.sum((xyv[1:, :] - xyv[:-1, :]) ** 2, axis=1))
+                    dxyv_nonzero = dxyv != 0
+                    xyv = xyv[np.concatenate(([True], dxyv_nonzero)), :]
+                    dxyv = dxyv[dxyv_nonzero]
                     dxyv = np.concatenate(([0], dxyv))
-
                     ssd = np.cumsum(dxyv)
                     ss0 = np.arange(1, np.ceil(ssd.max()) + 0.49, 0.49)
                     xnew = np.interp(ss0, ssd, xyv[:, 0]).round().astype(int)
                     ynew = np.interp(ss0, ssd, xyv[:, 1]).round().astype(int)
-
                     try:
                         indnew = np.ravel_multi_index((ynew, xnew), szz)
                     except ValueError:
                         print('annotation out of bounds')
                         continue
-
                     indnew = indnew[~np.isnan(indnew)].astype(int)
                     bwtypek.flat[indnew] = 1
-
                 bwtypek = binary_fill_holes(bwtypek > 0)
-                bwtypek = np.pad(bwtypek, 401, mode='constant', constant_values=0)
-                Jtmp[bwtypek > 0] = k
+                Jtmp[bwtypek == 1] = k
                 if not kpb:
-                    Jtmp[:401, :] = 0
-                    Jtmp[:, :401] = 0
+                    Jtmp[:400, :] = 0
+                    Jtmp[:, :400] = 0
                     Jtmp[-401:, :] = 0
                     Jtmp[:, -401:] = 0
-                J[k] = np.flatnonzero(Jtmp == k)
+                J[int(k) - 1] = np.flatnonzero(Jtmp == k)
 
             del bwtypek, Jtmp, xyz
             # format annotations to keep or remove whitespace
@@ -88,7 +87,8 @@ def save_annotation_mask(I,outpth,WS,umpix,TA,kpb=0):  #I is the image in matrix
             from PIL import Image
             Image.fromarray(np.uint8(J)).save(outpth + 'view_annotations_raw.tif')
     except:
-        J=np.zeros(I[:, :, 0].shape, dtype=int)
+        J=np.zeros(I.size)
+    return J
 
 
 pth = r'\\10.99.68.52\Kiemendata\Valentina Matos\LG HG PanIN project\Jaime\Python tests'
