81f0cf60476c6d31c1e09c14108b4558240d8170
Valentina <vmatosr1@jh.edu>
2024-04-29 14:15:59 -0400

    Updating deleted files

--- xml2struct.py
+++
@@ -1,9 +0,0 @@
-import xmltodict
-
-def xml2struct(file_path):
-    with open(file_path, 'r') as file:
-        xml_data = file.read()
-        xml_dict = xmltodict.parse(xml_data)
-    return xml_dict
-
-
