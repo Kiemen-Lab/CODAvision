713d70bec36acc6a70e4e5acd775c443402f68df
Valentina <vmatosr1@jh.edu>
2025-01-06 17:14:49 -0500

    trying weighted classification layer

--- base/backbones.py
+++ base/backbones.py
@@ -3,14 +3,16 @@ Author: Arrun Sivasubramanian (Johns Hopkins - Kiemen Lab)
 Date: November 01, 2024
 """
 
+import time
+import pickle
 import keras
-from keras import layers, models, applications, activations
+from keras import layers, models, applications
 import tensorflow as tf
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 import os
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 os.environ["TF_CPP_MIN_VLOG_LEVEL"] = "2"
 os.system("nvcc --version")
@@ -20,13 +22,29 @@ import GPUtil
 warnings.filterwarnings('ignore')
 
 import tensorflow as tf
+from tensorflow.keras.layers import Layer
 from tensorflow.keras import layers, Model
+
+class WeightedClassificationLayer(Layer):
+    def __init__(self, num_classes, class_weights, **kwargs):
+        super(WeightedClassificationLayer, self).__init__(**kwargs)
+        self.num_classes = num_classes
+        self.class_weights = class_weights
+
+    def build(self, input_shape):
+        self.conv = tf.keras.layers.Conv2D(self.num_classes, kernel_size=(1, 1), padding="same")
+
+    def call(self, inputs):
+        logits = self.conv(inputs)
+        weighted_logits = logits * self.class_weights
+        return weighted_logits
 
 
 class DeepLabV3Plus:
-    def __init__(self, input_size, num_classes):
+    def __init__(self, input_size, num_classes, class_weights=None):
         self.input_size = input_size
         self.num_classes = num_classes
+        self.class_weights = class_weights
 
     def convolution_block(self, block_input, num_filters=256, kernel_size=3, dilation_rate=1, use_bias=False):
         x = layers.Conv2D(num_filters, kernel_size=kernel_size, dilation_rate=dilation_rate, padding="same",
@@ -82,7 +100,10 @@ class DeepLabV3Plus:
             interpolation="bilinear",
         )(x)
 
-        outputs = layers.Conv2D(self.num_classes, kernel_size=(1, 1), padding="same")(x)
+        if self.class_weights is not None:
+            outputs = WeightedClassificationLayer(self.num_classes, self.class_weights)(x)
+        else:
+            outputs = layers.Conv2D(self.num_classes, kernel_size=(1, 1), padding="same")(x)
 
         model = Model(preprocessed, outputs, name='DeepLabV3_plus')
         return model
@@ -508,200 +529,19 @@ class CASe_UNet:
 
         return model
 
-class CoREAM2_SegNet:
-    def __init__(self, input_size, num_classes):
-        self.input_size = input_size
-        self.num_classes = num_classes
 
-    def downsampler(self, fmap, count):
-        for i in range(count):
-            fmap = layers.MaxPool2D(2, dtype='float32')(fmap)
-        return fmap
-
-    def upsampler(self, fmap, count):
-        for i in range(count):
-            fmap = layers.UpSampling2D(2, dtype='float32')(fmap)
-        return fmap
-
-    def residual_encoder(self, input_features, num_filters, layer, filter_size=[3, 5, 7]):
-        # input_copy = input_features
-        conv_1 = layers.Conv2D(num_filters, 3, padding='same')(input_features)
-        ln_1 = layers.LayerNormalization()(conv_1)
-        activation_enc = activations.gelu(ln_1)
-
-        conv1_1 = layers.Conv2D(num_filters, filter_size[0], padding='same', activation=layers.LeakyReLU(),
-                                dtype='float32', name=f'conv-e-{layer}_1_1')(input_features)
-        conv1_2 = layers.Conv2D(num_filters, filter_size[1], padding='same', activation=layers.LeakyReLU(),
-                                dtype='float32', name=f'conv-e-{layer}_1_2')(input_features)
-        conv1_3 = layers.Conv2D(num_filters, filter_size[2], padding='same', activation=layers.LeakyReLU(),
-                                dtype='float32', name=f'conv-e-{layer}_1_3')(input_features)
-
-        concat_12 = layers.Concatenate(dtype='float32', name=f'conc-e-{layer}_12')([conv1_1, conv1_2])
-        concat_13 = layers.Concatenate(dtype='float32', name=f'conc-e-{layer}_23')([conv1_2, conv1_3])
-        concat_23 = layers.Concatenate(dtype='float32', name=f'conv-e-{layer}_13')([conv1_1, conv1_3])
-
-        conv2_1 = layers.Conv2D(filter_size[0], 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                name=f'conv-e-{layer}_2_1')(concat_12)
-        conv2_2 = layers.Conv2D(filter_size[1], 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                name=f'conv-e-{layer}_2_2')(concat_23)
-        conv2_3 = layers.Conv2D(filter_size[2], 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                name=f'conv-e-{layer}_2_3')(concat_13)
-
-        concat_123 = layers.Concatenate(dtype='float32', name=f'conc-e-{layer}_123')([conv2_1, conv2_2, conv2_3])
-        conv_fin = layers.Conv2D(num_filters, 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                 name=f'conv-e-{layer}_fin')(concat_123)
-
-        add = layers.Add()([activation_enc, conv_fin])
-        ln_2 = layers.LayerNormalization()(add)
-        out = activations.gelu(ln_2)
-
-        maxpool_fin = layers.MaxPool2D(2, dtype='float32', name=f'maxpool_fin-{layer}')(out)
-        return maxpool_fin
-
-    def residual_decoder(self, input_features, num_filters, layer, filter_size=[3, 5, 7]):
-        conv_1 = layers.Conv2D(num_filters, 3, padding='same')(input_features)
-        ln_1 = layers.LayerNormalization()(conv_1)
-        activation_dec = activations.gelu(ln_1)
-
-        convt1_1 = layers.Conv2DTranspose(num_filters, filter_size[0], padding='same',
-                                          activation=layers.LeakyReLU(), dtype='float32',
-                                          name=f'convt-d-{layer}_1_1')(input_features)
-        convt1_2 = layers.Conv2DTranspose(num_filters, filter_size[1], padding='same',
-                                          activation=layers.LeakyReLU(), dtype='float32',
-                                          name=f'convt-d-{layer}_1_2')(input_features)
-        convt1_3 = layers.Conv2DTranspose(num_filters, filter_size[2], padding='same',
-                                          activation=layers.LeakyReLU(), dtype='float32',
-                                          name=f'convt-d-{layer}_1_3')(input_features)
-
-        concat_12 = layers.Concatenate(dtype='float32', name=f'conc-d-{layer}_12')([convt1_1, convt1_2])
-        concat_13 = layers.Concatenate(dtype='float32', name=f'conc-d-{layer}_23')([convt1_2, convt1_3])
-        concat_23 = layers.Concatenate(dtype='float32', name=f'conc-d-{layer}_13')([convt1_1, convt1_3])
-
-        conv2_1 = layers.Conv2D(filter_size[0], 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                name=f'conv-d-{layer}_2_1')(concat_12)
-        conv2_2 = layers.Conv2D(filter_size[1], 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                name=f'conv-d-{layer}_2_2')(concat_23)
-        conv2_3 = layers.Conv2D(filter_size[2], 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                name=f'conv-d-{layer}_2_3')(concat_13)
-
-        concat_123 = layers.Concatenate(dtype='float32', name=f'conc-d-{layer}_123')([conv2_1, conv2_2, conv2_3])
-
-        conv_fin = layers.Conv2D(num_filters, 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                 name=f'conv-d-{layer}_fin')(concat_123)
-
-        add = layers.Add()([activation_dec, conv_fin])
-        ln_2 = layers.LayerNormalization()(add)
-        out = activations.gelu(ln_2)
-
-        upsampling_fin = layers.UpSampling2D(2, dtype='float32', name=f'upsampling_fin-{layer}')(out)
-
-        return upsampling_fin
-
-    def pixel_attention(self, input_features, num_chans):
-        pointwise_conv = layers.GlobalAveragePooling2D()(input_features)
-        ln_layer = layers.BatchNormalization()(pointwise_conv)
-        prob_dist = layers.Softmax(axis=-1)(ln_layer)
-
-        low_lvl = layers.Conv2D(num_chans, 1, padding='same')(input_features)
-        mult_layer = layers.Multiply()([low_lvl, prob_dist])
-
-        add_layer = layers.Add()([mult_layer, low_lvl])
-
-        return add_layer
-
-    def skip_concat(self, down_skip_connection, up_skip_connection, num_filters, layer):
-        if (len(down_skip_connection) == 4):
-            skip_connection_d = layers.Concatenate(dtype='float32', name=f'conc-down-d-{layer}')(
-                [self.downsampler(down_skip_connection[0], 4), self.downsampler(down_skip_connection[1], 3),
-                 self.downsampler(down_skip_connection[2], 2), self.downsampler(down_skip_connection[3], 1)])
-        elif (len(down_skip_connection) == 3):
-            skip_connection_d = layers.Concatenate(dtype='float32', name=f'conc-down-d-{layer}')(
-                [self.downsampler(down_skip_connection[0], 3), self.downsampler(down_skip_connection[1], 2),
-                 self.downsampler(down_skip_connection[2], 1)])
-        elif (len(down_skip_connection) == 2):
-            skip_connection_d = layers.Concatenate(dtype='float32', name=f'conc-down-d-{layer}')(
-                [self.downsampler(down_skip_connection[0], 2), self.downsampler(down_skip_connection[1], 1)])
-        elif (len(down_skip_connection) == 1):
-            skip_connection_d = layers.Concatenate(dtype='float32', name=f'conc-down-d-{layer}')(
-                [self.downsampler(down_skip_connection[0], 1)])
-        else:
-            print("ERROR INITIALIZING DOWN SKIPS!!")
-
-        if (len(up_skip_connection) == 4):
-            skip_connection_u = layers.Concatenate(dtype='float32', name=f'conc-up-d-{layer}')(
-                [self.upsampler(up_skip_connection[0], 3), self.upsampler(up_skip_connection[1], 2),
-                 self.upsampler(up_skip_connection[2], 1), up_skip_connection[3]])
-        elif (len(up_skip_connection) == 3):
-            skip_connection_u = layers.Concatenate(dtype='float32', name=f'conc-up-d-{layer}')(
-                [self.upsampler(up_skip_connection[0], 2), self.upsampler(up_skip_connection[1], 1),
-                 up_skip_connection[2]])
-        elif (len(up_skip_connection) == 2):
-            skip_connection_u = layers.Concatenate(dtype='float32', name=f'conc-up-d-{layer}')(
-                [self.upsampler(up_skip_connection[0], 1), up_skip_connection[1]])
-        elif (len(up_skip_connection) == 1):
-            skip_connection_u = layers.Concatenate(dtype='float32', name=f'conc-up-d-{layer}')(
-                [up_skip_connection[0]])
-        else:
-            print("ERROR INITIALIZING UP SKIPS!!")
-
-        concat_123 = layers.Concatenate(dtype='float32', name=f'conc-ud-{layer}_123')(
-            [skip_connection_d, skip_connection_u])
-        conv_fin = layers.Conv2D(num_filters, 1, padding='same', activation=layers.LeakyReLU(), dtype='float32',
-                                 name=f'conv-s-{layer}_fin')(concat_123)
-
-        ln = layers.LayerNormalization()(conv_fin)
-        out = activations.gelu(ln)
-
-        return out
-
-    def build_model(self):
-        input_layer = layers.Input((self.input_size, self.input_size, 3))
-        e1 = self.residual_encoder(input_layer, 32, str(1))
-        e2 = self.residual_encoder(e1, 64, str(2))
-        e3 = self.residual_encoder(e2, 128, str(3))
-        e4 = self.residual_encoder(e3, 256, str(4))
-
-        c1 = self.skip_concat([e1, e2, e3], [e4], 256, str(1))
-        afm1 = self.pixel_attention(c1, 256)
-        d1 = self.residual_decoder(afm1, 128, str(1))
-
-        c2 = self.skip_concat([e1, e2], [e4, d1], 128, str(2))
-        afm2 = self.pixel_attention(c2, 128)
-        d2 = self.residual_decoder(afm2, 64, str(2))
-
-        c3 = self.skip_concat([e1], [e4, d1, d2], 64, str(3))
-        afm3 = self.pixel_attention(c3, 64)
-        d3 = self.residual_decoder(afm3, 32, str(3))
-
-        afm4 = self.pixel_attention(d3, 32)
-        d4 = self.residual_decoder(afm4, 16, str(5))
-
-        d_intout1 = layers.Conv2D(16, 3, padding='same', activation=layers.LeakyReLU(), name='feature_smoothen_1')(
-            d4)
-        d_intout2 = layers.Conv2D(16, 3, padding='same', activation=layers.LeakyReLU(), name='feature_smoothen_2')(
-            d_intout1)
-        d_out = layers.Conv2D(self.num_classes, kernel_size=1, padding='same', activation='softmax',
-                              name='segmaps')(d_intout2)
-
-        model = Model(inputs=input_layer, outputs=[d_out], name="CoREAM2_SegNet")
-        return model
-
-
-        # Instantiate and build
-def model_call(name, IMAGE_SIZE, NUM_CLASSES):
-    if (name == "UNet"):
+# Instantiate and build
+def model_call(name, IMAGE_SIZE, NUM_CLASSES, class_weights=None):
+    if name == "UNet":
         model = UNet(input_size=IMAGE_SIZE, num_classes=NUM_CLASSES).build_model()
-    elif (name == "DeepLabV3_plus"):
-        model = DeepLabV3Plus(input_size=IMAGE_SIZE, num_classes=NUM_CLASSES).build_model()
-    elif (name == "UNet3_plus"):
+    elif name == "DeepLabV3_plus":
+        model = DeepLabV3Plus(input_size=IMAGE_SIZE, num_classes=NUM_CLASSES, class_weights=class_weights).build_model()
+    elif name == "UNet3_plus":
         model = UNet3Plus(input_size=IMAGE_SIZE, num_classes=NUM_CLASSES).build_model()
-    elif (name == "TransUNet"):
+    elif name == "TransUNet":
         model = TransUNet(input_size=IMAGE_SIZE, num_classes=NUM_CLASSES).build_model()
-    elif (name == "CASe_UNet"):
+    elif name == "CASe_UNet":
         model = CASe_UNet(input_size=IMAGE_SIZE, num_classes=NUM_CLASSES).build_model()
-    elif (name == "CoREAM2_SegNet"):
-        model = CoREAM2_SegNet(input_size=IMAGE_SIZE, num_classes=NUM_CLASSES).build_model()
-
     else:
         raise ValueError(f'Incorrect Model Name / Pretrained model of {name} does not exist')
 
