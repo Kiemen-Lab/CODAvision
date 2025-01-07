7f9ea0e6b290b533e4b0355c0a81d24da86bfc5a
Valentina <vmatosr1@jh.edu>
2025-01-07 10:55:32 -0500

    fixed bug in setup.py with pydicom

--- setup.py
+++ setup.py
@@ -33,8 +33,8 @@ setup(
         'openslide-python==1.3.1',
         'openpyxl==3.1.2',
         'GPUtil==1.4.0',
-        'PySide6==6.5.1.1'
-        'pydicom==3.0.1',
+        'PySide6==6.5.1.1',
+        'pydicom',
         'fpdf==1.7.2',
     ],
     package_data={
