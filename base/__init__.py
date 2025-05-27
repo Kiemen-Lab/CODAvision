6198a67a1956e2a21a0a3a293be443a44e57054e
Tyler Newton <tnewton@jhu.edu>
2025-05-27 09:25:54 -0400

    update import for WSI2tif refactor

--- base/__init__.py
+++ base/__init__.py
@@ -20,7 +20,7 @@ from .data.tiles import create_training_tiles
 from .evaluation.image_quantification import quantify_images
 from .evaluation.object_quantification import quantify_objects
 from .evaluation.pdf_report import create_output_pdf
-from .WSI2tif import WSI2tif
+from .image.wsi import WSI2tif
 from .data.annotation import (
     load_annotation_data,
     save_annotation_mask,
