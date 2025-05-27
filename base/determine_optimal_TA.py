10d96d968d3fc2c4d943302e4f866aba1ace6ccc
Tyler Newton <tnewton@jhu.edu>
2025-05-27 09:37:44 -0400

    update image loading calls and improve formatting with whitespace

--- base/determine_optimal_TA.py
+++ base/determine_optimal_TA.py
@@ -1,14 +1,17 @@
-
+import os
 import numpy as np
 from glob import glob
-import os
 import sys
 import pickle
 from PySide6 import QtGui, QtWidgets, QtCore
 from PySide6.QtCore import Qt, QRect
 import cv2
+from PIL import Image
+from base.image import load_image_with_fallback
 
-def determine_optimal_TA(pthim,pthtestim, numims, redo):
+
+
+def determine_optimal_TA(pthim, pthtestim, numims, redo):
     from gui.components.dialogs import (
         Ui_choose_area, Ui_disp_crop, Ui_choose_TA,
         Ui_choose_images_reevaluated, Ui_use_current_TA
@@ -16,6 +19,7 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
     CT0 = 205
     szz = 600
     print('Answer prompt pop-up window regarding tissue masks to proceed')
+
     class disp_whole_im(QtWidgets.QMainWindow):
         def __init__(self, shape, rsf, parent=None):
 
@@ -26,15 +30,16 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             self.setWindowTitle("Click on a location at the edge of tissue and whitespace")
             self.clicked_position = None
             self.ui.whole_im.setCursor(QtCore.Qt.CrossCursor)
-            self.setGeometry(30+np.round(1500-shape[1]*rsf)/2, 30+np.round(800-shape[0]*rsf)/2,
-                             np.round(shape[1]*rsf), np.round(shape[0]*rsf))
+            self.setGeometry(30 + np.round(1500 - shape[1] * rsf) / 2, 30 + np.round(800 - shape[0] * rsf) / 2,
+                             np.round(shape[1] * rsf), np.round(shape[0] * rsf))
+
         def update_image(self, im0, rsf):
             image_array = np.ascontiguousarray(im0)
             height, width = image_array.shape[:2]
             bytes_per_line = 3 * width
             qimage = QtGui.QImage(image_array.data, width, height, bytes_per_line, QtGui.QImage.Format_RGB888)
             pixmap = QtGui.QPixmap.fromImage(qimage)
-            self.ui.whole_im.setGeometry(0, 0, np.round(im0.shape[1]*rsf), np.round(im0.shape[0]*rsf))
+            self.ui.whole_im.setGeometry(0, 0, np.round(im0.shape[1] * rsf), np.round(im0.shape[0] * rsf))
             self.ui.whole_im.setPixmap(pixmap.scaled(
                 self.ui.whole_im.size(),
                 Qt.KeepAspectRatio,
@@ -46,14 +51,15 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             if self.ui.whole_im.geometry().contains(event.position().toPoint()):
                 self.clicked_position = event.position() - QtCore.QPointF(self.ui.whole_im.geometry().topLeft())
                 self.close()
+
     def display_image(im0, rsf):
         app = QtWidgets.QApplication.instance()
         if app is None:
             app = QtWidgets.QApplication(sys.argv)
 
-        window = disp_whole_im(im0.shape,rsf)
+        window = disp_whole_im(im0.shape, rsf)
         window.show()
-        window.update_image(im0,rsf)
+        window.update_image(im0, rsf)
         app.exec()
         return window.clicked_position
 
@@ -68,14 +74,14 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             main_layout = QtWidgets.QVBoxLayout()
             self.setCentralWidget(central_widget)
             self.ui.text.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
-            main_layout.addWidget(self.ui.text,alignment=Qt.AlignCenter)
+            main_layout.addWidget(self.ui.text, alignment=Qt.AlignCenter)
             self.ui.cropped.setFixedSize(szz, szz)
             self.ui.cropped.setAlignment(Qt.AlignCenter)
             self.ui.cropped.setSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed)
             main_layout.addWidget(self.ui.cropped, alignment=Qt.AlignCenter)
             button_layout = QtWidgets.QHBoxLayout()
             button_layout.setSpacing(5)
-            button_height = 90  # or any height you prefer
+            button_height = 90
             self.ui.looks_good.setFixedHeight(button_height)
             self.ui.new_loc.setFixedHeight(button_height)
             self.ui.looks_good.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
@@ -149,8 +155,8 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             images_layout.setSpacing(4)
             self.ui.medium_im.setMinimumSize(300, 300)
             self.ui.medium_im.setMaximumSize(300, 300)
-            self.ui.TA_im.setMinimumSize(300,300)
-            self.ui.TA_im.setMaximumSize(300,300)
+            self.ui.TA_im.setMinimumSize(300, 300)
+            self.ui.TA_im.setMaximumSize(300, 300)
             mode_widget = QtWidgets.QWidget()
             mode_layout = QtWidgets.QVBoxLayout(mode_widget)
             self.ui.change_mode.setMinimumSize(155, 50)
@@ -219,7 +225,7 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             self.ui.TA_selection.setValue(self.CT0)
 
         def update_slider(self):
-            self.ui.slider_label.setText("Current threshold value: "+str(self.ui.TA_selection.value()))
+            self.ui.slider_label.setText("Current threshold value: " + str(self.ui.TA_selection.value()))
             self.on_change_TA()
 
         def on_apply(self):
@@ -272,7 +278,7 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
         def update_image(self, szz, cropped):
             if self.mode == 'H&E':
                 image_array_medium = np.ascontiguousarray(cropped)
-                image_array = np.ascontiguousarray(((cropped[:,:,1]>self.CT0)*255).astype(np.uint8))
+                image_array = np.ascontiguousarray(((cropped[:, :, 1] > self.CT0) * 255).astype(np.uint8))
             else:
                 image_array_medium = np.ascontiguousarray(cropped)
                 image_array = np.ascontiguousarray(((cropped[:, :, 1] < self.CT0) * 255).astype(np.uint8))
@@ -287,7 +293,8 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
                 Qt.SmoothTransformation
             ))
             height, width = image_array.shape[:2]
-            qimage = QtGui.QImage(image_array.data, width, height, image_array.strides[0], QtGui.QImage.Format_Grayscale8)
+            qimage = QtGui.QImage(image_array.data, width, height, image_array.strides[0],
+                                  QtGui.QImage.Format_Grayscale8)
             pixmap = QtGui.QPixmap.fromImage(qimage)
             self.ui.TA_im.setPixmap(pixmap.scaled(
                 self.ui.TA_im.size(),
@@ -295,12 +302,11 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
                 Qt.SmoothTransformation
             ))
 
-
     def select_TA(szz, cropped, CT0, mode):
         app = QtWidgets.QApplication.instance()
         if app is None:
             app = QtWidgets.QApplication(sys.argv)
-        window = chooseTA(szz,CT0, mode)
+        window = chooseTA(szz, CT0, mode)
         window.show()
         window.update_image(szz, cropped)
         app.exec()
@@ -326,7 +332,7 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             browse_layout.addWidget(self.ui.browse_PB)
             main_layout.addLayout(browse_layout)
             self.ui.image_LW.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-            self.ui.image_LW.setMinimumSize(550,100)
+            self.ui.image_LW.setMinimumSize(550, 100)
             main_layout.addWidget(self.ui.image_LW)
             buttons_layout = QtWidgets.QVBoxLayout()
             self.ui.delete_PB.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
@@ -357,7 +363,7 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             if event.spontaneous():
                 self.images = []
                 QtWidgets.QMessageBox.information(self, 'Info',
-                                              'Keeping previous tissue mask evaluation')
+                                                  'Keeping previous tissue mask evaluation')
 
         def on_delete_image(self):
             delete_row = self.ui.image_LW.currentRow()
@@ -369,8 +375,8 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
             self.ui.image_LW.takeItem(delete_row)
 
         def on_browse(self):
-            file_path,_ = QtWidgets.QFileDialog.getOpenFileName(self, "Select Image to Reevaluate",
-                                                                pthim,"TIFF Images (*.tif *.tiff)")
+            file_path, _ = QtWidgets.QFileDialog.getOpenFileName(self, "Select Image to Reevaluate",
+                                                                 pthim, "TIFF Images (*.tif *.tiff)")
             if file_path:
                 if os.path.isfile(file_path):
                     self.ui.image_LE.setText(file_path)
@@ -386,13 +392,13 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
                     self.ui.image_LE.setText('')
             else:
                 QtWidgets.QMessageBox.warning(self, 'Warning',
-                                             'Selected image does not exist')
+                                              'Selected image does not exist')
 
         def on_apply(self):
             i = 0
             for image in self.images:
                 self.images[i] = os.path.basename(image)
-                i +=1
+                i += 1
             self.close()
 
         def on_apply_all(self):
@@ -413,13 +419,13 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
     if not imlist:
         jpg_files = glob(os.path.join(pthim, "*.jpg"))
         if jpg_files:
-            imlist.extend(jpg_files)  # Add full paths of JPGs to list
+            imlist.extend(jpg_files) # Add full paths of JPGs to list
         jpg_files = glob(os.path.join(pthtestim, "*.jpg"))
         if jpg_files:
-            imtestlist.extend(jpg_files)  # Add full paths of JPGs to list
+            imtestlist.extend(jpg_files)
         png_files = glob(os.path.join(pthim, '*.png'))
         if png_files:
-            imlist.extend(png_files)
+            imlist.extend(png_files) # Add full paths of JPGs to list
         png_files = glob(os.path.join(pthtestim, '*.png'))
         if png_files:
             imtestlist.extend(png_files)
@@ -428,7 +434,7 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
     print('   ')
     i = 0
     for image in imlist:
-        imlist[i] = image[len(pthim)+1:]
+        imlist[i] = image[len(pthim) + 1:]
         i += 1
     imlist.extend(imtestlist)
 
@@ -437,8 +443,8 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
     mode = 'H&E'
     if not os.path.exists(outpath):
         os.makedirs(outpath)
-    if os.path.isfile(os.path.join(outpath,'TA_cutoff.pkl')):
-        if numims>0:
+    if os.path.isfile(os.path.join(outpath, 'TA_cutoff.pkl')):
+        if numims > 0:
             keep_TA = not redo
             if keep_TA:
                 print('   Optimal cutoff already chosen, skip this step')
@@ -466,8 +472,8 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
                         imlist = redo_list
     if mode == 'Grayscale':
         CT0 = 50
-    if numims>0:
-        numims = min(numims,len(imlist))
+    if numims > 0:
+        numims = min(numims, len(imlist))
         imlist = np.random.choice(imlist, size=numims, replace=False)
         print(f'Evaluating {numims} randomly selected images to choose a good whitespace detection...')
         average_TA = True
@@ -479,54 +485,53 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
     count = 0
     for nm in imlist:
         count += 1
-        if len(nm)>len(pthtestim) and nm[:len(pthtestim)]==pthtestim:
-            print(f'    Loading image {count} of {numims}: {nm[len(pthtestim)+1:]}')
-            im0 = cv2.imread(nm)
+        if len(nm) > len(pthtestim) and nm[:len(pthtestim)] == pthtestim:
+            print(f'    Loading image {count} of {numims}: {nm[len(pthtestim) + 1:]}')
+            im0 = load_image_with_fallback(nm)
         else:
             print(f'    Loading image {count} of {numims}: {nm}')
-            im0 = cv2.imread(os.path.join(pthim,nm))
-        im0 = im0[:,:,::-1]
+            im0 = load_image_with_fallback(os.path.join(pthim, nm))
+        im0 = im0[:, :, ::-1]
         print('     Image loaded')
-        rsf = min(1500/im0.shape[1], 780/im0.shape[0])
+        rsf = min(1500 / im0.shape[1], 780 / im0.shape[0])
         do_again = 1
         while do_again == 1:
-            click = display_image(im0,rsf)
+            click = display_image(im0, rsf)
             x = click.x()
             y = click.y()
-            x_norm = int(np.round(x/rsf))
-            y_norm = int(np.round(y/rsf))
-            cropped_temp = im0[:,:,:]
+            x_norm = int(np.round(x / rsf))
+            y_norm = int(np.round(y / rsf))
+            cropped_temp = im0[:, :, :]
             cropped = np.array([])
-            if 2*szz>im0.shape[0] and 2*szz>im0.shape[1]:
-                max_dim = np.max([im0.shape[0],im0.shape[1]])
+            if 2 * szz > im0.shape[0] and 2 * szz > im0.shape[1]:
+                max_dim = np.max([im0.shape[0], im0.shape[1]])
                 pad_x = (max_dim - im0.shape[0]) // 2
                 pad_y = (max_dim - im0.shape[1]) // 2
-                # Ensure even padding (if odd difference, add extra on one side)
                 pad_x1, pad_x2 = pad_x, pad_x + (max_dim - im0.shape[0]) % 2
                 pad_y1, pad_y2 = pad_y, pad_y + (max_dim - im0.shape[1]) % 2
                 cropped = np.pad(im0, ((pad_x1, pad_x2), (pad_y1, pad_y2), (0, 0)), mode='constant')
-            elif 2*szz>im0.shape[0]:
-                pad_x = (2*szz - im0.shape[0]) // 2
-                pad_x1, pad_x2 = pad_x, pad_x + (2*szz - im0.shape[0]) % 2
+            elif 2 * szz > im0.shape[0]:
+                pad_x = (2 * szz - im0.shape[0]) // 2
+                pad_x1, pad_x2 = pad_x, pad_x + (2 * szz - im0.shape[0]) % 2
                 padded = np.pad(im0, ((pad_x1, pad_x2), (0, 0), (0, 0)), mode='constant')
-                cropped_temp = padded[y_norm-szz:y_norm+szz,x_norm-szz:x_norm+szz, :]
-            elif 2*szz>im0.shape[1]:
-                pad_y = (2*szz - im0.shape[1]) // 2
-                pad_y1, pad_y2 = pad_y, pad_y + (2*szz - im0.shape[1]) % 2
-                padded = np.pad(im0, ((0,0), (pad_y1, pad_y2), (0, 0)), mode='constant')
-                cropped_temp = padded[y_norm-szz:y_norm+szz,x_norm-szz:x_norm+szz, :]
-            if y_norm<szz:
-                cropped_temp = cropped_temp[0: 2*szz, :, :]
-            elif y_norm+szz>im0.shape[0]:
-                cropped_temp = cropped_temp[-2*szz:, :, :]
+                cropped_temp = padded[y_norm - szz:y_norm + szz, x_norm - szz:x_norm + szz, :]
+            elif 2 * szz > im0.shape[1]:
+                pad_y = (2 * szz - im0.shape[1]) // 2
+                pad_y1, pad_y2 = pad_y, pad_y + (2 * szz - im0.shape[1]) % 2
+                padded = np.pad(im0, ((0, 0), (pad_y1, pad_y2), (0, 0)), mode='constant')
+                cropped_temp = padded[y_norm - szz:y_norm + szz, x_norm - szz:x_norm + szz, :]
+            if y_norm < szz:
+                cropped_temp = cropped_temp[0: 2 * szz, :, :]
+            elif y_norm + szz > im0.shape[0]:
+                cropped_temp = cropped_temp[-2 * szz:, :, :]
             else:
-                cropped_temp = cropped_temp[y_norm-szz:y_norm+szz,:,:]
-            if x_norm<szz:
-                cropped_temp = cropped_temp[:,0: 2*szz, :]
-            elif x_norm+szz>im0.shape[1]:
-                cropped_temp = cropped_temp[:,-2*szz:, :]
+                cropped_temp = cropped_temp[y_norm - szz:y_norm + szz, :, :]
+            if x_norm < szz:
+                cropped_temp = cropped_temp[:, 0: 2 * szz, :]
+            elif x_norm + szz > im0.shape[1]:
+                cropped_temp = cropped_temp[:, -2 * szz:, :]
             else:
-                cropped_temp = cropped_temp[:,x_norm-szz:x_norm+szz,:]
+                cropped_temp = cropped_temp[:, x_norm - szz:x_norm + szz, :]
             if cropped.size == 0:
                 cropped = cropped_temp
             do_again = check_region(szz, cropped)
@@ -539,5 +544,5 @@ def determine_optimal_TA(pthim,pthtestim, numims, redo):
         else:
             cts = {**cts, nm: CT0}
     with open(os.path.join(outpath, 'TA_cutoff.pkl'), 'wb') as f:
-        pickle.dump({'cts':cts,'imlist':imlist, 'mode': mode, 'average_TA': average_TA}, f)
+        pickle.dump({'cts': cts, 'imlist': imlist, 'mode': mode, 'average_TA': average_TA}, f)
     return
